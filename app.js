389f0e536c335fb951f91a8dc64efb1de73c51c8
Veerendra Nayari <veerendranayari6362@gmail.com>
2026-06-25 17:59:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+// Serve static files
+app.use(express.static('public'));
+
+// Route for /
 app.get('/', (req, res) => {
-  res.send('Node.js Application Running');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000, () => {
